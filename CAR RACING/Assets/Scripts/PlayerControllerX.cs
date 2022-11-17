d3f27583718ed55277dd576d865e4458ad225504
Matiire <114078222+Matiire@users.noreply.github.com>
2022-11-17 15:29:18 +0000

    :100:

--- CAR RACING/Assets/Scripts/PlayerControllerX.cs
+++ CAR RACING/Assets/Scripts/PlayerControllerX.cs
@@ -15,9 +15,18 @@ public class PlayerControllerX : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
+    {	    if(Input.GetKey(KeyCode.W)) {
+            	transform.Translate(Vector3.forward * Time.deltaTime * 50);
+            }
+            if(Input.GetKey(KeyCode.S)) {
+	                	transform.Translate(Vector3.back * Time.deltaTime * 50);
+            }
+            if(Input.GetKey(KeyCode.D)) {
+	    	                	transform.Rotate(Vector3.up * Time.deltaTime * 50);
+            }
+            if(Input.GetKey(KeyCode.A)) {
+	    	                	transform.Rotate(Vector3.down * Time.deltaTime * 50);
+            }
        
     }
 }
